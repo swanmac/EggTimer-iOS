9fff67cab77a890dd4e18861892d22c6d7c12dcb
Greg Swan <gmswan99@gmail.com>
2023-09-14 19:38:46 -0400

    Connect egg times with buttons

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 10
-    let hardTime = 12
-
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector:
+            #selector(updateTimer), userInfo:nil, repeats: true)
+      }
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        }
     }
     
 }
