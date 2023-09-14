a3d07bac2d83975b49f32e992cc789004ef03335
Greg Swan <gmswan99@gmail.com>
2023-09-14 14:56:15 -0400

    add cook times as constants

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 10
+    let hardTime = 12
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
     }
     
 }
