e8ca5f167f3ec4e028b72ec18874d17ea96fd0b7
Greg Swan <gmswan99@gmail.com>
2023-09-18 08:57:37 -0400

    update timer title

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var secondsRemaining = 60
     
@@ -31,6 +33,9 @@ class ViewController: UIViewController {
         if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
         }
     }
     
